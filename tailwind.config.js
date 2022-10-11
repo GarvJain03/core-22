3fb189cc38a0f6fa0fff6adab7fe3f1b84de35d0
GarvJain03 <garvj03@gmail.com>
2022-10-11 22:31:53 +0530

    chore: configure tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,9 +14,10 @@ module.exports = {
         "nav-txt-color": "#333868",
         "primary-text": "#525168",
         "secondary-text": "#828282",
+        "blue-bg": "#3C3A5F",
       },
       fontFamily: {
-        sans: ["Poppins", ...defaultTheme.fontFamily.sans],
+        sans: ["Karla", ...defaultTheme.fontFamily.sans],
       },
     },
   },
