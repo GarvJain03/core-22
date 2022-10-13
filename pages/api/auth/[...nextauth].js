2c2085bcf6920151133cb67a084356339f0b6cd3
GarvJain03 <garvj03@gmail.com>
2022-10-13 23:41:55 +0530

    commit

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,13 +1,9 @@
 import NextAuth from "next-auth";
 import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
-import clientPromise from "../../../lib/mongodb";
+// import clientPromise from "../../../lib/mongodb";
 import GoogleProvider from "next-auth/providers/google";
 
 export default NextAuth({
-  //The adapter to the database we will use to store the data
-  adapter: MongoDBAdapter(clientPromise, {
-    databaseName: "tony-airways",
-  }), //The providers are the autentication method
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
@@ -16,4 +12,5 @@ export default NextAuth({
   ],
 
   secret: "thisisasupersecretsecret",
+  debug: true,
 });
