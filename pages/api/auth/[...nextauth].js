c46e6947e46d42930084a80761cb039d881a9cfb
GarvJain03 <garvj03@gmail.com>
2022-10-13 23:20:52 +0530

    fix errors

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -4,10 +4,16 @@ import clientPromise from "../../../lib/mongodb";
 import GoogleProvider from "next-auth/providers/google";
 
 export default NextAuth({
+  //The adapter to the database we will use to store the data
+  adapter: MongoDBAdapter(clientPromise, {
+    databaseName: "tony-airways",
+  }), //The providers are the autentication method
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
+
+  secret: "thisisasupersecretsecret",
 });
